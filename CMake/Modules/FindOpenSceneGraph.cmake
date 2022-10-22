39383917639fc8524c27b4691c6448cc5c3d3015
timpatt <timpatt@users.noreply.github.com>
2022-10-22 15:07:43 +1100

    wip

---
+++ CMake/Modules/FindOpenSceneGraph.cmake
@@ -0,0 +1,22 @@
+find_package(OpenSceneGraph CONFIG)
+
+if (OPENSCENEGRAPH_FOUND OR OpenSceneGraph_FOUND)
+	message(STATUS "Found OSG")
+	if (NOT DEFINED OPENSCENEGRAPH_INCLUDE_DIRS)
+		if (DEFINED openscenegraph_INCLUDE_DIRS)
+			# Conan defaults to using lowercase - convert it to the case used in the "standard" CMake find_package module
+			message(VERBOSE "OpenSceneGraph found, but resulting ")
+			set(OPENSCENEGRAPH_FOUND TRUE)
+			set(OPENSCENEGRAPH_VERSION  "${openscenegraph_VERSION_STRING}")
+			set(OPENSCENEGRAPH_INCLUDE_DIRS "${openscenegraph_INCLUDE_DIRS}")
+			set(OPENSCENEGRAPH_LIBRARIES "${openscenegraph_LIBRARIES}")
+			set(OPENSCENEGRAPH_DEFINITIONS "${openscenegraph_DEFINITIONS}")
+		else()
+			message(FATAL_ERROR "OpenSceneGraph found, but cannot find expected variables")
+		endif()
+	endif()
+else()
+	message(STATUS "Couldn't find OSG")
+	# Fallback to default find module
+	include("${CMAKE_ROOT}/Modules/FindOpenSceneGraph.cmake")
+endif()
