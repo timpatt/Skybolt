d58c1ad41c53e2ff1a73b72830fdb6561649ed74
prograda user on PROG4-Ubuntu <contact@prograda.com>
2026-08-07 03:32:26 +0000

    uv and uv python installs are now cached

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+# The root of any newly-created named volumes is always owned by root:root.
+# Update it to the correct uid:gid.  Only really required after volume creation.
+sudo chown -R vscode:vscode /home/vscode/.conan2
+# uv caches stuff in here
+sudo chown -R vscode:vscode /home/vscode/.cache
+
 # FIXME: These get installed *every* time we start the container which is a bit annoying...
 uv tool install conan==2.31.1
 uv tool install mkdocs==1.6.1 --with mkdocs-material==9.7.7 --with 'mkdocstrings[python]==1.0.6'
-
-# The root of a newly-created named volume is always owned by root:root.
-# Update it to the correct uid:gid.  Only really required after volume creation.
-sudo chown -R vscode:vscode /home/vscode/.conan2
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
